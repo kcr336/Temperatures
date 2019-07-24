fd269065d871ba1485f31d9874e4f5155174e33f
kcr336 <46976285+kcr336@users.noreply.github.com>
2019-07-24 17:57:25 -0500

    Add files via upload

--- temperatures.java
+++ temperatures.java
@@ -1,5 +1,4 @@
 import static java.lang.System.out;
-import java.util.Scanner;
 public class temperatures {
 
     public static void main(String[] args) {
@@ -12,36 +11,50 @@ public class temperatures {
 
 	out.println("Temperature Calculater");
 	out.println();
-	String times[]={"7am","3pm","7pm","3am"};
+	String times[]={"7am: ","3pm: ","7pm: ","3am: "};
+	String days[]= {"Sun: ","Mon: ","Tues: ","Wed: ","Thurs: ","Fri: ","Sat: "};
 	out.println("The data provided are:");
 	for(int row=0; row<temps.length; row++){
-		out.println(times[row]);
+		out.print(times[row]);
 		for(int column=0; column<7; column++)
 		{
 			out.print(" "+temps[row][column]+" ");
-		}
+		}out.print('\n');
 	}
-	out.println();
+	out.println('\n');
 	out.println("Based on the data, the following are the average temperatures for the week.");
-	out.println();
-	out.println("Sun: "+(temps[0][0]+temps[1][0]+temps[2][0]+temps[3][0])/4);
-	out.println("Mon: "+(temps[0][1]+temps[1][1]+temps[2][1]+temps[3][1])/4);
-	out.println("Tues: "+(temps[0][2]+temps[1][2]+temps[2][2]+temps[3][2])/4);
-	out.println("Wed: "+(temps[0][3]+temps[1][3]+temps[2][3]+temps[3][3])/4);
-	out.println("Thu: "+(temps[0][4]+temps[1][4]+temps[2][4]+temps[3][4])/4);
-	out.println("Fri: "+(temps[0][5]+temps[1][5]+temps[2][5]+temps[3][5])/4);
-	out.println("Sat: "+(temps[0][6]+temps[1][6]+temps[2][6]+temps[3][6])/4);
-	out.println();
-	out.println("7 am: "+(temps[0][0]+temps[0][1]+temps[0][2]+temps[0][3]+temps[0][4]+temps[0][5]+temps[0][6])/7);
-	out.println("3 pm: "+(temps[1][0]+temps[1][1]+temps[1][2]+temps[1][3]+temps[1][4]+temps[1][5]+temps[1][6])/7);
-	out.println("7 pm: "+(temps[2][0]+temps[2][1]+temps[2][2]+temps[2][3]+temps[2][4]+temps[2][5]+temps[2][6])/7);
-	out.println("3 am: "+(temps[3][0]+temps[3][1]+temps[3][2]+temps[3][3]+temps[3][4]+temps[3][5]+temps[3][6])/7);
+	out.println('\n');
+	for (int c=0; c<days.length; c++){
+		out.print(days[c]);
+		int sum=0;
+		for (int r=0; r<times.length; r++){
+			sum += temps[r][c];
+		}
+		out.print(" "+sum/times.length);
+		out.print('\n');
+	}
+    out.println("");
+	for (int r=0; r<times.length; r++){
+		out.print(times[r]);
+		int sum1=0;
+		for (int c=0; c<days.length; c++){
+		    sum1 += temps[r][c];
+        }
+		out.print(sum1/days.length);
+		out.print('\n');
+	}
 	out.println();
 	out.println("The final average temperature for the week was: ");
 	out.println();
-	out.println("Overall: "+(temps[0][0]+temps[0][1]+temps[0][2]+temps[0][3]+temps[0][3]+temps[0][4]+temps[0][5]+temps[0][6]+
-			temps[1][0]+temps[1][1]+temps[1][2]+temps[1][3]+temps[1][4]+temps[1][5]+temps[1][6]+
-			temps[2][0]+temps[2][1]+temps[2][2]+temps[2][3]+temps[2][4]+temps[2][5]+temps[2][6]+
-			temps[3][0]+temps[3][1]+temps[3][2]+temps[3][3]+temps[3][4]+temps[3][5]+temps[3][6])/28);
+	int average = days.length*times.length;
+	int sum2=0;
+	for (int r=0; r<times.length; r++) {
+		for (int c = 0; c < days.length; c++) {
+			sum2 += temps[r][c];
+		}
+		}
+		out.print("Overall: "+sum2/average);
+		out.print('\n');
+	}
 }
-}
+
